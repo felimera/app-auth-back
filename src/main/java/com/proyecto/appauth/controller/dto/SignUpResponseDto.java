ebee778ba4d5ebc549277ecd175b4aa643e0437d
andres.mera <feli041089@gmail.com>
2024-03-27 15:20:02 -0500

    feature/security : Nuevos dtos.

---
+++ src/main/java/com/proyecto/appauth/controller/dto/SignUpResponseDto.java
@@ -0,0 +1,15 @@
+package com.proyecto.appauth.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpResponseDto {
+    private UserDto userDto;
+    private LoginResponseDto loginResponseDto;
+}
