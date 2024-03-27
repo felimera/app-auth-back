bf449dc6674cb4579c2f2fb809fe3606ff2bcc7f
andres.mera <feli041089@gmail.com>
2024-03-27 12:56:39 -0500

    feature/auth : Creacion del service de User.

---
+++ src/main/java/com/proyecto/appauth/service/UserService.java
@@ -0,0 +1,9 @@
+package com.proyecto.appauth.service;
+
+import com.proyecto.appauth.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    boolean createUser(User user);
+}
