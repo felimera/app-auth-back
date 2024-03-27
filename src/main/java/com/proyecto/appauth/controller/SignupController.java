d9ab11c07993bddb60875e375e059557b19a3f6b
andres.mera <feli041089@gmail.com>
2024-03-27 13:12:51 -0500

    feature/exception : Agregando validacion y uso de las ecpciones.

--- src/main/java/com/proyecto/appauth/controller/SignupController.java
+++ src/main/java/com/proyecto/appauth/controller/SignupController.java
@@ -3,10 +3,12 @@ package com.proyecto.appauth.controller;
 import com.proyecto.appauth.controller.dto.SignUpDto;
 import com.proyecto.appauth.controller.mapper.UserMapper;
 import com.proyecto.appauth.model.User;
+import com.proyecto.appauth.model.exception.ExceptionResponseMessage;
 import com.proyecto.appauth.service.UserService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -29,14 +31,13 @@ public class SignupController {
 
         User user = UserMapper.INSTANCE.toSignUp(signUpDto);
         boolean isUserCreated = userService.createUser(user);
-        return ResponseEntity.ok().build();
-//        if (isUserCreated)
-//            return ResponseEntity
-//                    .status(HttpStatus.CREATED)
-//                    .body(ExceptionResponseMessage.builder().code(String.valueOf(HttpStatus.CREATED.value())).message("User created succesfully.").build());
-//        else
-//            return ResponseEntity
-//                    .status(HttpStatus.BAD_REQUEST)
-//                    .body(ExceptionResponseMessage.builder().code(String.valueOf(HttpStatus.BAD_REQUEST.value())).message("Failed created user.").build());
+        if (isUserCreated)
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(ExceptionResponseMessage.builder().code(String.valueOf(HttpStatus.CREATED.value())).message("User created succesfully.").build());
+        else
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(ExceptionResponseMessage.builder().code(String.valueOf(HttpStatus.BAD_REQUEST.value())).message("Failed created user.").build());
     }
 }
