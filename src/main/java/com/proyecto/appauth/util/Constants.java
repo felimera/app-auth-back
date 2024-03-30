87620bc0f8ccb5f97e54ab759814df476338b0b2
andres.mera <feli041089@gmail.com>
2024-03-30 10:00:54 -0500

    feature/test : Modificaciones por el test de UserServiceImplTest.java.

--- src/main/java/com/proyecto/appauth/util/Constants.java
+++ src/main/java/com/proyecto/appauth/util/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
     public static final String T = "T";
     public static final String I = "I";
     public static final String A = "A";
+    public static final String U = "U";
 
     public static final String REGULARPHRASE_DATE = "^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$";
     public static final String MESSAGE_NOT_FOUND = "No results were found.";
