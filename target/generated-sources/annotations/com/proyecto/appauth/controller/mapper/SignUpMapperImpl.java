38c38a29d2a2f73c3e1cfa100d1599f90101b6b3
andres.mera <feli041089@gmail.com>
2024-04-01 08:48:29 -0500

    releases/deployment : Generando el Jar

--- target/generated-sources/annotations/com/proyecto/appauth/controller/mapper/SignUpMapperImpl.java
+++ target/generated-sources/annotations/com/proyecto/appauth/controller/mapper/SignUpMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-01T07:50:30-0500",
+    date = "2024-04-01T08:45:27-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 public class SignUpMapperImpl implements SignUpMapper {
